938ca1fe76d8067fa3ff0b71225c4f6ebc9ca0a8
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-11-24 18:00:26 +0200

    nm

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views import View
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
@@ -13,3 +16,37 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  
     context_object_name = 'library'
+
+# User Login View
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Change 'home' to your desired redirect URL
+        return render(request, 'login.html', {'form': form})
+
+# User Logout View
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return render(request, 'logout.html')
+
+# User Registration View
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Change 'home' to your desired redirect URL
+        return render(request, 'register.html', {'form': form})
