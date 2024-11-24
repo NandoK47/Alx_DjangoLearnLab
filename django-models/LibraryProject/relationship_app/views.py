8889ee014ed5ebff9fe3a6a92f52b13d89788ab2
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-11-24 23:20:58 +0200

    nm

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -57,20 +57,20 @@ def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
 @user_passes_test(is_admin, login_url='/login/')  # Redirect unauthorized users to login
 def admin_view(request):
-    return render(request, 'admin_view.html', {'user': request.user})
+    return render(request, 'relationship_app/admin_view.html', {'user': request.user})
 
 def is_librarian(user):
      return user.is_authenticated and user.userprofile.role == 'Librarian'
 @user_passes_test(is_librarian, login_url='/login/')  # Redirect unauthorized users to login
 def librarian_view(request):
-    return render(request, 'librarian_view.html', {'user': request.user})
+    return render(request, 'relationship_app/librarian_view.html', {'user': request.user})
 
 
 def is_member(user):
     return user.is_authenticated and user.userprofile.role == 'Member'
 @user_passes_test(is_member, login_url='/login/')  # Redirect unauthorized users to login
 def member_view(request):
-    return render(request, 'member_view.html', {'user': request.user})
+    return render(request, 'relationship_app/member_view.html', {'user': request.user})
 
 
 # Admin view
