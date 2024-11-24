0d7a7c36a89a781c40c24e8e1b3e629fa30c66e2
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-11-24 18:57:11 +0200

    nm

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from .views import LoginView, LogoutView, RegisterView
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(template_name='register.html'), name='register'),
+    path('register/', views.register, name='register'),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,11 +20,11 @@ class LibraryDetailView(DetailView):
 # User Login View
 class LoginView(View):
     def get(self, request):
-        form = AuthenticationForm()
+        form = 'AuthenticationForm'()
         return render(request, 'login.html', {'form': form})
 
     def post(self, request):
-        form = AuthenticationForm(data=request.POST)
+        form = 'AuthenticationForm'(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
