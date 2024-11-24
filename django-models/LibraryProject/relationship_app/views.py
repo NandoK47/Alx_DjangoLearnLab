caef9acc1250a19142a2ea370d98094009e1a47e
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-11-24 18:05:09 +0200

    nm

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.views import View
 from django.views.generic.detail import DetailView
 from .models import Book
