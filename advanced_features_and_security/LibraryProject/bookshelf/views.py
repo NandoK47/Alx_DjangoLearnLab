b5d284c8ef2a6bf65eae86713d7dc10b3ab68020
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-11-29 19:50:40 +0200

    nm

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Article
+from .models import Book
 
 # Create your views here.
 
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+# View to add a new book (requires permission)
+@permission_required('bookshelf.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = 'BookForm'(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = 'BookForm'()
+    return render(request, 'add_book.html', {'form': form})
+
+# View to edit an existing book (requires permission)
+@permission_required('bookshelf.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = 'BookForm'(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = 'BookForm'(instance=book)
+    return render(request, 'edit_book.html', {'form': form})
+
+# View to delete a book (requires permission)
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'delete_book.html', {'book': book})
 
