bf4835863be42c106e4f8f5be718bfb02340b06d
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-12-06 20:22:30 +0200

    mm

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,9 +6,9 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import BasePermission
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework
-from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView
 
 # Create your views here.
 
