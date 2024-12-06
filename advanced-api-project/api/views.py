94dfa4ad7e028a8f825ea775ab86f504acf4e316
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-12-06 11:43:26 +0200

    mm

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import BasePermission
 
 # Create your views here.
+
+class BookListView(generics.ListAPIView):
+     queryset = Book.objects.all()
+     serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+     queryset = Book.objects.all()
+     serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+     queryset = Book.objects.all()
+     serializer_class = BookSerializer
+     permission_classes = [permissions.IsAuthenticated]
+     def perform_create(self, serializer):
+           if serializer.validated_data['publication_year'] > 2024:
+            raise ValidationError("Publication year cannot be in the future.")
+           serializer.save(author=self.request.user)
+
+class BookUpdateView(generics.UpdateAPIView):
+     queryset = Book.objects.all()
+     serializer_class = BookSerializer
+     permission_classes = [permissions.IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+     queryset = Book.objects.all()
+     serializer_class = BookSerializer
+     permission_classes = [permissions.IsAuthenticated]
+
+class IsAdminOrReadOnly(BasePermission):
+     def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return request.user and request.user.is_staff
