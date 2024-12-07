e1dc7e66a874698d184d6813200cd7511d906bf1
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-12-07 21:20:11 +0200

    mm

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
-from .models import Book
+from .models import Book, Author
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -13,14 +13,18 @@ class TestBookAPI(TestCase):
         self.client.login(username='testuser', password='password123')
         self.book_url = '/api/books/' 
 
+        self.author1 = Author.objects.create(name='Author A')
+        self.author2 = Author.objects.create(Name='Author B')
+
         self.book1 = Book.objects.create(title='Book One', author='Author A', published_date='2023-01-01')
         self.book2 = Book.objects.create(title='Book Two', author='Author B', published_date='2023-02-01')
 
     def test_create_book(self):
-        data = {'title': 'New Book', 'author': 'Author C', 'published_date': '2024-01-01'}
+        data = {'title': 'New Book', 'author': 'self.author1.id', 'published_date': '2024-01-01'}
         response = self.client.post(self.book_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['title'], 'New Book')
+        self.assertEqual(response.data['author'], self.author1.id)
 
     def test_get_books(self):
         response = self.client.get(self.book_url)
@@ -38,7 +42,7 @@ class TestBookAPI(TestCase):
         response = self.client.get(f'{self.book_url}?author=Author A')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['author'], 'Author A')
+        self.assertEqual(response.data[0]['author'], self.author1.id)
 
     def test_order_books_by_title(self):
         response = self.client.get(f'{self.book_url}?ordering=title')
