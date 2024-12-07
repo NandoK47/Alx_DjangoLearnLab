46ddae90a46bbae8c6f469d32af10e9386f61732
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-12-07 20:51:17 +0200

    mm

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
-        'rest_framework.OrderingFilter',
-        'rest_framework.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
     ]
 }
