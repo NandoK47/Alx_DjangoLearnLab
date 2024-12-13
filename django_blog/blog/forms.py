d414f23fee1f8158991041b6d7f536549067d862
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-12-13 17:11:25 +0200

    bn

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -18,7 +18,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-        widgets = {'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'})}
+        widgets = {
+            'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'}),
+        }
     def save(self, commit=True):
         instance = super().save(commit=False)
         if commit:
