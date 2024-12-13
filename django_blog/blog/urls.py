3566339cce21786a53b5367d9018ccba0feb925c
NandoK47 <niyonkurujeanluc01@gmail.com>
2024-12-13 17:25:36 +0200

    bn

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import PostListView, PostCreateView, PostUpdateView, PostDeleteView, PostDetailView
 from .import views
-from .views import CommentCreateView, CommentUpdateView, CommentDeleteView, TagDetailView
-
+from .views import CommentCreateView, CommentUpdateView, CommentDeleteView, TagDetailView, PostByTagListView
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
@@ -18,5 +17,6 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit_comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
-     path('tags/<str:tag_name>/', TagDetailView.as_view(), name='tag_detail'),
+    path('tags/<str:tag_name>/', TagDetailView.as_view(), name='tag_detail'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts_by_tag'),
 ]
