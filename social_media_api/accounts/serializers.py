8f4c199dc25d022eb99e47e825bc1192585eddd6
NandoK47 <niyonkurujeanluc01@gmail.com>
2025-01-03 18:25:35 +0200

    vbn

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,29 +1,12 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
 # Get the custom user model
-User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # CharField for password with write-only property
-
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
-        extra_kwargs = {
-            'password': {'write_only': True},  # Ensure the password is not returned in the response
-        }
+        model = CustomUser
+        fields = ['id','username', 'email', 'bio', 'profile_picture', 'followers']
 
-    def create(self, validated_data):
-        """
-        Create a new user instance using the provided validated data.
-        """
-        # Use the custom `create_user` method of the user model
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
-        )
-        return user
+        
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,28 +1,33 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 from rest_framework import status
-from django.contrib.auth import authenticate
-from .serializers import UserSerializer, TokenSerializer
+
 
 # Create your views here.
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
+        user = CustomUser.objects.filter(username=username).first()
+        if user and user.check_password(password):
+            token, _ = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
