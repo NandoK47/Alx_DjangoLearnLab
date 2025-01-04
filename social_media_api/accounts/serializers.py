3a2ec04db16d4a8029bdad942fadda991d7625ff
NandoK47 <niyonkurujeanluc01@gmail.com>
2025-01-04 13:46:35 +0200

    bn

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -14,6 +14,12 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ['id','username', 'email', 'bio', 'profile_picture', 'followers']
 
     def create(self, validated_data):
-        user = get_user_model.objects.create_user()
+        user = get_user_model.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
         Token.objects.create(user=user)
         return user
