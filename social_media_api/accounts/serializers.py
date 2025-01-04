f04535438bfb30be79fa57aa7fb61a3de3794739
NandoK47 <niyonkurujeanluc01@gmail.com>
2025-01-04 14:39:12 +0200

    bn

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
-
+from django.contrib.auth import authenticate
 # Get the custom user model
 
 User = get_user_model()
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class CustomUserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ['id','username', 'email', 'bio', 'profile_picture', 'followers']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         
@@ -21,3 +22,19 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+        
+        token, created = Token.objects.get_or_create(user=user)
+        data['token'] = token.key
+        return data
