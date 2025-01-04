e99bb2456d0b3250e4a6ef9285114a1592c9ca3e
NandoK47 <niyonkurujeanluc01@gmail.com>
2025-01-04 13:34:40 +0200

    bn

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,13 +7,13 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ['id','username', 'email', 'bio', 'profile_picture', 'followers']
 
     def create(self, validated_data):
-        user = User.objects.create_user(username=validated_data['username'], email=validated_data.get('email'), password=validated_data['password'], bio=validated_data.get('bio', ''), profile_picture=validated_data.get('profile_picture', None),)
+        user = User.objects.create_user()
         Token.objects.create(user=user)
         return user
